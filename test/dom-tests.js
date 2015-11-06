e80f29a0fd97cea373039785a58754557c488ccf
Rolf Erik Lekang <me@rolflekang.com>
2015-11-06 19:16:57 +0100

    chore: Add eslint config

--- test/dom-tests.js
+++ test/dom-tests.js
@@ -9,7 +9,7 @@ xdescribe('DOM nodes', () => {
   it('should throw if it does not find valid xpath in element', () => {
     const element = document.createElement('blink');
     expect(() => {
-      expect(element).to.have.xpath('//h1')
+      expect(element).to.have.xpath('//h1');
     }).to.throw('Expected element to have xpath \'//h1\'');
   });
 });
--- test/react-tests.js
+++ test/react-tests.js
@@ -7,22 +7,22 @@ const {expect} = chai;
 describe('React components', () => {
   it('should find valid xpath in react component', () => {
     const component = TestUtils.renderIntoDocument(<blink>hi</blink>);
-    expect(component).to.have.xpath('//blink')
+    expect(component).to.have.xpath('//blink');
   });
 
-  describe("when it does not find valid xpath in react component", () => {
+  describe('when it does not find valid xpath in react component', () => {
     it('should throw', () => {
       const component = TestUtils.renderIntoDocument(<blink>hi</blink>);
       expect(() => {
-        expect(component).to.have.xpath('//h1')
+        expect(component).to.have.xpath('//h1');
       }).to.throw('to have xpath \'//h1\'');
     });
 
     it('should throw with outerHTML of the component', () => {
       const component = TestUtils.renderIntoDocument(<blink>hi</blink>);
       expect(() => {
-        expect(component).to.have.xpath('//h1')
+        expect(component).to.have.xpath('//h1');
       }).to.throw('<blink data-reactid=".2">hi</blink>');
     });
-  })
+  });
 });
