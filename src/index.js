6578877ea48481fd6d4fb54345eba884b875a9db
Rolf Erik Lekang <me@rolflekang.com>
2016-02-09 23:03:45 +0100

    fix: Use have-xpath as dependency

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
 /* eslint-env browser */
-import {findSingleNode, getFindDOMNode} from './helpers';
+import { haveXpath, getFindDOMNode } from 'have-xpath';
 
 let findDOMNode = findDOMNode || (global && global.findDOMNode);
 
-function haveDomNodeWithXpath(domNode, expression) {
-  document.body.appendChild(domNode);
-  const xpathNode = findSingleNode(expression, domNode.parentNode);
-  document.body.removeChild(domNode);
-
-  return xpathNode !== null;
-}
-
-export default function haveXpath(Chai) {
+export default function chaiHaveXpath(Chai) {
   Chai.Assertion.addMethod('xpath', function evaluateXpath(xpath) {
     findDOMNode = findDOMNode || getFindDOMNode();
 
     const domNode = findDOMNode(this._obj);
 
     this.assert(
-      haveDomNodeWithXpath(domNode, xpath),
-      'Expected "' + domNode.outerHTML + '" to have xpath \'' + xpath + '\'',
-      'Expected "' + domNode.outerHTML + '" to not have xpath \'' + xpath + '\''
+      haveXpath(this._obj, xpath),
+      `Expected "${domNode.outerHTML}" to have xpath '${xpath}'`,
+      `Expected "${domNode.outerHTML}" to not have xpath '${xpath}'`
     );
   });
 }
