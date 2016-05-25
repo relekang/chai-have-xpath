31e4f574be89437179fefa97d9881ead0af8b162
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2016-05-25 13:33:28 +0000

    feat: add pretty printing of html (#62)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 /* eslint-env browser */
 import { haveXpath, getFindDOMNode } from 'have-xpath';
+import pretty from 'pretty';
 
 export default function chaiHaveXpath(Chai) {
   Chai.Assertion.addMethod('xpath', function evaluateXpath(xpath) {
     const findDOMNode = getFindDOMNode();
 
     const domNode = findDOMNode(this._obj);
+    const html = pretty(domNode.outerHTML);
 
     this.assert(
       haveXpath(this._obj, xpath),
-      `Expected "${domNode.outerHTML}" to have xpath '${xpath}'`,
-      `Expected "${domNode.outerHTML}" to not have xpath '${xpath}'`
+      `Expected \n\n${html}\n\n  to have xpath '${xpath}'`,
+      `Expected \n\n${html}\n\n  to not have xpath '${xpath}'`
     );
   });
 }
